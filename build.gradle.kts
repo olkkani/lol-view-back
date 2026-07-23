b122ed20a297d3dfee5df45b3deeda9d59c2096c
jin <ahn.chanjin.96@gmail.com>
2026-07-23 15:54:24 +0900

    :tada: config application.yaml

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,14 @@
+
 plugins {
-    kotlin("jvm") version "2.2.21"
-    kotlin("plugin.spring") version "2.2.21"
-    id("org.springframework.boot") version "4.0.6"
-    id("io.spring.dependency-management") version "1.1.7"
-    kotlin("plugin.jpa") version "2.2.21"
+    alias(libs.plugins.kotlin.jvm)
+    alias(libs.plugins.kotlin.spring)
+    alias(libs.plugins.spring.boot)
+    alias(libs.plugins.dependency.management)
+    alias(libs.plugins.ksp)
+    alias(libs.plugins.kotlin.jpa)
+    alias(libs.plugins.jooq.monosoul)
+    alias(libs.plugins.ktlint)
+    alias(libs.plugins.detekt)
 }
 
 group = "io.olkkani"
@@ -12,7 +17,7 @@ description = "lol-view-back"
 
 java {
     toolchain {
-        languageVersion = JavaLanguageVersion.of(24)
+        languageVersion = JavaLanguageVersion.of(25)
     }
 }
 
@@ -21,46 +26,55 @@ repositories {
 }
 
 dependencies {
-    implementation("org.springframework.boot:spring-boot-h2console")
-    implementation("org.springframework.boot:spring-boot-starter-batch")
-    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-    implementation("org.springframework.boot:spring-boot-starter-data-rest")
-    implementation("org.springframework.boot:spring-boot-starter-flyway")
-    implementation("org.springframework.boot:spring-boot-starter-hateoas")
-    implementation("org.springframework.boot:spring-boot-starter-jooq")
-    implementation("org.springframework.boot:spring-boot-starter-security")
-    implementation("org.springframework.boot:spring-boot-starter-security-oauth2-client")
-    implementation("org.springframework.boot:spring-boot-starter-validation")
-    implementation("org.springframework.boot:spring-boot-starter-webclient")
-    implementation("org.springframework.boot:spring-boot-starter-webflux")
-    implementation("org.springframework.boot:spring-boot-starter-webmvc")
-    implementation("org.springframework.boot:spring-boot-starter-webservices")
-    implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
-    implementation("org.flywaydb:flyway-database-postgresql")
-    implementation("org.jetbrains.kotlin:kotlin-reflect")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
-    implementation("tools.jackson.module:jackson-module-kotlin")
-    developmentOnly("org.springframework.boot:spring-boot-devtools")
-    developmentOnly("org.springframework.boot:spring-boot-docker-compose")
-    runtimeOnly("com.h2database:h2")
-    runtimeOnly("org.postgresql:postgresql")
-    annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
-    testImplementation("org.springframework.boot:spring-boot-starter-batch-test")
-    testImplementation("org.springframework.boot:spring-boot-starter-data-jpa-test")
-    testImplementation("org.springframework.boot:spring-boot-starter-data-rest-test")
-    testImplementation("org.springframework.boot:spring-boot-starter-flyway-test")
-    testImplementation("org.springframework.boot:spring-boot-starter-hateoas-test")
-    testImplementation("org.springframework.boot:spring-boot-starter-jooq-test")
-    testImplementation("org.springframework.boot:spring-boot-starter-security-oauth2-client-test")
-    testImplementation("org.springframework.boot:spring-boot-starter-security-test")
-    testImplementation("org.springframework.boot:spring-boot-starter-validation-test")
-    testImplementation("org.springframework.boot:spring-boot-starter-webclient-test")
-    testImplementation("org.springframework.boot:spring-boot-starter-webflux-test")
-    testImplementation("org.springframework.boot:spring-boot-starter-webmvc-test")
-    testImplementation("org.springframework.boot:spring-boot-starter-webservices-test")
-    testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
-    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test")
-    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    implementation(libs.bundles.kotlin)
+    implementation(libs.bundles.common)
+    implementation(libs.bundles.spring) {
+        exclude(
+            group = "org.springframework.boot",
+            module = "spring-boot-starter-tomcat",
+        )
+    }
+    implementation(libs.bundles.spring.security)
+    developmentOnly(libs.spring.devtools)
+    testImplementation(libs.bundles.test)
+    testImplementation(libs.bundles.spring.test)
+
+    implementation(libs.bundles.kotlin.coroutines)
+    testImplementation(libs.bundles.kotlin.coroutines.test)
+
+    implementation(libs.bundles.persistence)
+    implementation(libs.bundles.persistence.database)
+    implementation(libs.p6spy)
+    testImplementation(libs.bundles.persistence.test.testcontainer)
+    developmentOnly(libs.bundles.persistence.database.embedded)
+    ksp("org.springframework.boot:spring-boot-configuration-processor")
+    jooqCodegen(libs.postgresql)
+//    implementation("org.springframework.boot:spring-boot-starter-batch")
+//    implementation("org.springframework.boot:spring-boot-starter-data-rest")
+//    implementation("org.springframework.boot:spring-boot-starter-hateoas")
+//    implementation("org.springframework.boot:spring-boot-starter-security-oauth2-client")
+//    implementation("org.springframework.boot:spring-boot-starter-webclient")
+//    implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
+//    developmentOnly("org.springframework.boot:spring-boot-docker-compose")
+
+
+//    annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
+//    testImplementation("org.springframework.boot:spring-boot-starter-batch-test")
+//    testImplementation("org.springframework.boot:spring-boot-starter-data-jpa-test")
+//    testImplementation("org.springframework.boot:spring-boot-starter-data-rest-test")
+//    testImplementation("org.springframework.boot:spring-boot-starter-flyway-test")
+//    testImplementation("org.springframework.boot:spring-boot-starter-hateoas-test")
+//    testImplementation("org.springframework.boot:spring-boot-starter-jooq-test")
+//    testImplementation("org.springframework.boot:spring-boot-starter-security-oauth2-client-test")
+//    testImplementation("org.springframework.boot:spring-boot-starter-security-test")
+//    testImplementation("org.springframework.boot:spring-boot-starter-validation-test")
+//    testImplementation("org.springframework.boot:spring-boot-starter-webclient-test")
+//    testImplementation("org.springframework.boot:spring-boot-starter-webflux-test")
+//    testImplementation("org.springframework.boot:spring-boot-starter-webmvc-test")
+//    testImplementation("org.springframework.boot:spring-boot-starter-webservices-test")
+//    testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
+//    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test")
+//    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
 kotlin {
@@ -77,4 +91,36 @@ allOpen {
 
 tasks.withType<Test> {
     useJUnitPlatform()
+}
+
+val jooqGeneratedOutput = project.layout.buildDirectory.dir("generated-jooq") // 경로 변수화
+sourceSets {
+    main {
+        kotlin.srcDirs(jooqGeneratedOutput)
+    }
+}
+tasks.named("clean") {
+    doLast {
+        jooqGeneratedOutput.get().asFile.deleteRecursively()
+    }
+}
+tasks {
+    generateJooqClasses {
+        schemas.set(listOf("public"))
+        basePackageName.set("org.jooq.generated")
+        migrationLocations.setFromFilesystem("src/main/resources/db/migration")
+        outputDirectory.set(project.layout.buildDirectory.dir("generated-jooq"))
+        flywayProperties.put("flyway.placeholderReplacement", "false")
+        includeFlywayTable.set(true)
+        outputSchemaToDefault.add("public")
+        schemaToPackageMapping.put("public", "model")
+
+        usingJavaConfig {
+            /* "this" here is the org.jooq.meta.jaxb.Generator configure it as you please */
+        }
+    }
+}
+
+configurations.all {
+    exclude(group = "org.slf4j", module = "slf4j-simple")
 }
